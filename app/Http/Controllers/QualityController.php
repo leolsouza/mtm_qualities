ad3daef5d0b377927be22e5f6a5626d25e9956b5
Leonardo Lima <eng.leolsouza@hotmail.com>
2022-06-03 17:07:50 -0300

    first

---
+++ app/Http/Controllers/QualityController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreQualityRequest;
+use App\Http\Requests\UpdateQualityRequest;
+use App\Models\Quality;
+
+class QualityController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Quality::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreQualityRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreQualityRequest $request, Quality $quality)
+    {
+        $quality = Quality::create($request->validated());
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Quality  $quality
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Quality $quality)
+    {
+        return $quality->load('users');
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateQualityRequest  $request
+     * @param  \App\Models\Quality  $quality
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateQualityRequest $request, Quality $quality)
+    {
+        $response = $quality->update($request->validated());
+
+        return $quality;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Quality  $quality
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Quality $quality)
+    {
+        $quality->delete();
+    }
+}
---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreUserRequest;
+use App\Http\Requests\UpdateUserRequest;
+use App\Models\User;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return User::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreUserRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreUserRequest $request, User $user)
+    {
+        $user = User::create($request->validated());
+
+        return $user;
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function show(User $user)
+    {
+        return $user->load('qualities');
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateUserRequest  $request
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateUserRequest $request, User $user)
+    {
+        $response = $user->update($request->validated());
+
+        return $user;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(User $user)
+    {
+        $user->delete();
+    }
+}
